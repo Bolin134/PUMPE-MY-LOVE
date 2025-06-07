4a26945caaa2f253f18c6276d3062e6514a52867
Bolin134 <114965109+Bolin134@users.noreply.github.com>
2025-06-06 18:31:57 -0600

    Update cartjs.js

--- cartjs.js
+++ cartjs.js
@@ -4,3 +4,10 @@ const heart = document.getElementById('heart');
     heart.addEventListener('click', () => {
       card.classList.add('open');
     });
+
+document.getElementById('heart').addEventListener('click', function () {
+        const music = document.getElementById('bg-music');
+        music.play().catch((err) => {
+        console.log("El navegador bloqueó la reproducción automática. Se necesita interacción.");
+    });
+});
